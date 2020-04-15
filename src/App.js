0c01c6f5aab7113287aebd5c449211337b66d287
thuyet-gobear <thuyet@gobear.com>
2020-04-15 20:46:00 +0700

    update text in app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          By there!<br/>
+          Hi there again!<br/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
